e94da6db779e36aaa46cc88563b46c9de29369aa
busipatianil <155179624+busipatianil@users.noreply.github.com>
2025-05-20 10:19:07 +0530

    Delete Deploying a staticweb to s3 using Iac directory

--- Deploying a staticweb to s3 using Iac/output.tf
+++
@@ -1,4 +0,0 @@
-output "website_url" {
-  value = aws_s3_bucket.static_site.website_endpoint
-  description = "URL of the static website"
-}
--- Deploying a staticweb to s3 using Iac/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Unique name for your S3 bucket"
-  default     = "my-static-website-demo-123456"
-}
